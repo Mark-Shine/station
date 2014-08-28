da2b178a21f4966431d115e7f370f3bc1b812811
Shiyi <wb-shangzhihai@taobao.com>
2014-08-28 11:12:14 +0800

    添加当前Ip

--- cohost/context_processors.py
+++ cohost/context_processors.py
@@ -1,14 +1,18 @@
 # -*- coding: utf-8 *-*
 from django.contrib.auth.models import User
 from urllib2 import quote
+from cohost.utils import get_client_ip
+
 
 
 def common(request):
     user = request.user
+    user_ip = get_client_ip(request)
     if not isinstance(user, User):
         return {}
     context = dict(
         USER_NAME=user.username,
+        USER_IP=user_ip,
         )
     return context
 
--- cohost/utils.py
+++ cohost/utils.py
@@ -222,6 +222,14 @@ def put_into_ippool(ips, area_name=u'其他'):
         map(build_area_ip(area_name), range(IpRange[0], IpRange[1]+1))
 
 
+def get_client_ip(request):
+    x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+    if x_forwarded_for:
+        ip = x_forwarded_for.split(',')[0]
+    else:
+        ip = request.META.get('REMOTE_ADDR')
+    return ip
+
 # def fff():
 #     datas = Data.objects.all()
 #     for d in datas:
